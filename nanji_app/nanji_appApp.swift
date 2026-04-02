28804218951789601ad83a0bf4a52964a371b951
yeeun <Yy0839@naver.com>
2026-04-02 17:00:12 +0900

    first commit

---
+++ nanji_app/nanji_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanji_appApp.swift
+//  nanji_app
+//
+//  Created by electrozone on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct nanji_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
